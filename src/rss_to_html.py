03a0e128768bf79b06b688c4f792016772dcfc00
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2026-01-13 14:06:16 -0800

    Update rss_to_html.py

--- src/rss_to_html.py
+++ src/rss_to_html.py
@@ -108,6 +108,7 @@ def generate_html_base(title: str):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{title}</title>
     <link rel=\"stylesheet\" href=\"style.css\">
+    <script src=\"script.js\"></script>
 </head>
     <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.cssn\">
 <body>\n"""
