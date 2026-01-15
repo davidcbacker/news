04da34663461c4adff1500039bbd0fafbb824cc6
David Backer <20198171+davidcbacker@users.noreply.github.com>
2026-01-15 18:25:34 -0500

    satisfy html linter

--- src/rss_to_html.py
+++ src/rss_to_html.py
@@ -107,9 +107,9 @@ def generate_html_base(title: str):
     <meta charset=\"UTF-8\">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{title}</title>
-    <script type=\"text/javascript\" src=\"script.js\"></script>
+    <script src=\"script.js\"></script>
     <link rel=\"stylesheet\" href=\"style.css\">
-    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css\" integrity=\"sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==\" crossorigin=\"anonymous\" referrerpolicy=\"no-referrer\" />
+    <link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css\" integrity=\"sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==\" crossorigin=\"anonymous\" referrerpolicy=\"no-referrer\"></link>
 </head>
 <body>\n"""
     return html_base
@@ -147,7 +147,7 @@ def generate_top_nav_bar(current_page: str):
         ("technology.html", "Technology")
     ]
     nav_bar = """        <ul class=\"navbar\">
-            <a class=\"icon\" href=\"javascript:void(0);\" class=\"icon\" onclick=\"myFunction()\">
+            <a class=\"icon\" href=\"javascript:void(0);\" onclick=\"myFunction()\">
                 <i class=\"fa fa-bars\"></i>
             </a>\n"""
 
