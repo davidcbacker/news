9603ea5b17f78f3c66cce39dcaba1059c55c593e
davidcbacker <20198171+davidcbacker@users.noreply.github.com>
2026-02-03 04:59:24 -0500

    Update rss_to_html.py

--- src/rss_to_html.py
+++ src/rss_to_html.py
@@ -450,7 +450,7 @@ def generate_security_html(max_news_items):
         feed_url=sans_internet_storm_center_rss_url,
         max_news_items=max_news_items
     )
-    security_html += generate_html_section(
+    security_html += generate_reddit_technology_html_section(
         section_title="Troy Hunt Breaches",
         section_url="https://troyhunt.com/",
         feed_url=troy_hunt_breaches_rss_url,
